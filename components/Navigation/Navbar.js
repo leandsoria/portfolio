ab0e4c163dafd1b834ce4f9950786178a6ee13d3
leandsoria <lean.dsoria@gmail.com>
2022-10-05 03:17:11 -0300

    Updating navbar logo

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -13,6 +13,7 @@ const Navbar = () => {
             layout="fill"
             objectFit="contain"
             objectPosition="left"
+            priority="true"
           />
         </div>
       </div>
