94cc84fa852a90e612a49be0e3b11584d26eda83
leandsoria <lean.dsoria@gmail.com>
2022-10-05 02:59:03 -0300

    SEO Added

---
+++ components/SEO/Header.js
@@ -0,0 +1,114 @@
+import Head from 'next/head';
+
+const Header = (prop) => {
+  return (
+    <div>
+      <Head>
+        <title>Leandro Soria | Front-End Developer</title>
+        <meta name="title" content="Leandro Soria | Front-End Developer" />
+        <meta name="description" content="Hi, welcome to my portfolio!" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta
+          property="og:title"
+          content="Leandro Soria | Front-End Developer"
+        />
+        <meta
+          property="og:description"
+          content="Hi, welcome to my portfolio!"
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta
+          property="twitter:title"
+          content="Leandro Soria | Front-End Developer"
+        />
+        <meta
+          property="twitter:description"
+          content="Hi, welcome to my portfolio!"
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+        <link
+          rel="icon"
+          sizes="192x192"
+          type="image/png"
+          href="/favicon/android-icon-192x192-seochecker-805.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180-seochecker-805.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicon/apple-icon-152x152-seochecker-805.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicon/apple-icon-144x144-seochecker-805.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicon/apple-icon-120x120-seochecker-805.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicon/apple-icon-114x114-seochecker-805.png"
+        />
+        <link
+          rel="icon"
+          sizes="96x96"
+          type="image/png"
+          href="/favicon/favicon-96x96-seochecker-805.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/favicon/apple-icon-76x76-seochecker-805.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/favicon/apple-icon-72x72-seochecker-805.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/favicon/apple-icon-60x60-seochecker-805.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/favicon/apple-icon-57x57-seochecker-805.png"
+        />
+        <link
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+          href="/favicon/favicon-32x32-seochecker-805.png"
+        />
+        <link
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+          href="/favicon/favicon-16x16-seochecker-805.png"
+        />
+      </Head>
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,12 @@ import Experience from '../components/Sections/Experience';
 import Skills from '../components/Sections/Languajes/Skills';
 import Projects from '../components/Sections/Projects';
 import Connect from '../components/Contact/Connect';
+import Header from '../components/SEO/Header';
 
 const Home = () => {
   return (
     <Fragment>
+      <Header />
       <Opening />
       <About />
       <Skills />
