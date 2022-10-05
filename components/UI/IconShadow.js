dc086d28f6032f888845a343ecd1e11742d5a1fc
leandsoria <lean.dsoria@gmail.com>
2022-10-05 02:20:28 -0300

    third commit, margins on Skills fixed

--- components/UI/IconShadow.js
+++ components/UI/IconShadow.js
@@ -1,6 +1,6 @@
 const IconShadow = (prop) => {
   return (
-    <div className="w-20 lg:basis-[calc(25%_-_2.5rem)] relative before:bg-violet-300 before:rounded-[25%] before:w-20 before:h-12 before:absolute before:blur-[6px] before:[transform:translateX(-50%)_translateY(7.5rem)_perspective(12rem)_rotateX(40deg)] before:left-[50%] before:opacity-0 hover:before:opacity-40 before:duration-500 text-center lg:px-8">
+    <div className="w-20 md:basis-[calc(25%_-_2.5rem)] relative before:bg-violet-300 before:rounded-[25%] before:w-20 before:h-12 before:absolute before:blur-[6px] before:[transform:translateX(-50%)_translateY(7.5rem)_perspective(12rem)_rotateX(40deg)] before:left-[50%] before:opacity-0 hover:before:opacity-40 before:duration-500 text-center lg:px-8">
       {prop.children}
     </div>
   );
