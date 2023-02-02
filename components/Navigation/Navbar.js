cb3a0535ef48766a349b85c44719f5538b4ff8cb
Lean <lean.dsoria@gmail.com>
2023-02-02 03:58:21 -0300

    Fix navbar bug :bug:

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -6,29 +6,27 @@ import Slide from 'react-reveal/Slide';
 const Navbar = () => {
   return (
     <nav className="flex flex-row w-full justify-between lg:justify-around px-[6%] py-6">
-      <Slide top>
-        <div className="w-2/3 lg:w-28 relative ">
-          <div className="w-28 h-28 ">
-            <Image
-              src="/logo/lean-logo.png"
-              alt="Leandro Soria's Logo"
-              layout="fill"
-              objectFit="contain"
-              objectPosition="left"
-              priority={true}
-            />
-          </div>
+      <div className="w-2/3 lg:w-28 relative ">
+        <div className="w-28 h-28 ">
+          <Image
+            src="/logo/lean-logo.png"
+            alt="Leandro Soria's Logo"
+            layout="fill"
+            objectFit="contain"
+            objectPosition="left"
+            priority={true}
+          />
         </div>
-        <Sidemenu />
-        <div className="hidden lg:flex justify-end items-center w-2/3">
-          <ul className="flex ">
-            <NavbarItem urlLink="#about">About me</NavbarItem>
-            <NavbarItem urlLink="#projects">Projects</NavbarItem>
-            <NavbarItem urlLink="#experience">Experience</NavbarItem>
-            <NavbarItem urlLink="#connect">Let&apos;s Connect!</NavbarItem>
-          </ul>
-        </div>
-      </Slide>
+      </div>
+      <Sidemenu />
+      <div className="hidden lg:flex justify-end items-center w-2/3">
+        <ul className="flex ">
+          <NavbarItem urlLink="#about">About me</NavbarItem>
+          <NavbarItem urlLink="#projects">Projects</NavbarItem>
+          <NavbarItem urlLink="#experience">Experience</NavbarItem>
+          <NavbarItem urlLink="#connect">Let&apos;s Connect!</NavbarItem>
+        </ul>
+      </div>
     </nav>
   );
 };
