33957feece3248705088ffd73fe848d2f5c74eb4
leandsoria <lean.dsoria@gmail.com>
2023-05-16 23:07:56 -0300

    Work on navbar :construction:

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -1,14 +1,38 @@
+import IconSocial from '../UI/IconSocial';
+import Github from '../../public/icons/svg/github_social_icon.svg';
+import LinkedIn from '../../public/icons/svg/linkedin_social_icon.svg';
 import NavbarItem from './NavbarItems';
 import Sidemenu from './Sidemenu';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 60) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
-    <nav className="flex flex-row w-full justify-between lg:justify-around px-[6%] py-6">
-      <div className="w-2/3 lg:w-28 relative ">
-        <div className="w-28 h-28 ">
+    <nav
+      className={`flex flex-row w-full justify-between lg:justify-around px-[6%] z-[900] fixed ${
+        isScrolled ? 'py-2 bg-gray-100' : 'py-6'
+      } transition-all duration-300`}
+    >
+      <div className="w-2/3 lg:w-36 relative ">
+        <div className={`w-28 h-28 `}>
           <Image
-            src="/logo/lean-logo.png"
+            src="/logo/lean-logo_dark.png"
             alt="Leandro Soria's Logo"
             layout="fill"
             objectFit="contain"
@@ -24,6 +48,18 @@ const Navbar = () => {
           <NavbarItem urlLink="#projects">Projects</NavbarItem>
           <NavbarItem urlLink="#experience">Experience</NavbarItem>
           <NavbarItem urlLink="#connect">Let&apos;s Connect!</NavbarItem>
+          <li>
+            <IconSocial urlLink={'https://github.com/leandsoria'}>
+              <Github className="text-white fill-violet-800 group-hover:fill-violet-400 duration-500 relative " />
+            </IconSocial>
+          </li>
+          <li>
+            <IconSocial
+              urlLink={'https://www.linkedin.com/in/leandro-s-528388136/'}
+            >
+              <LinkedIn className="text-white fill-violet-800 group-hover:fill-violet-400 duration-500 relative  " />
+            </IconSocial>
+          </li>
         </ul>
       </div>
     </nav>
--- components/Sections/Opening/Opening.js
+++ components/Sections/Opening/Opening.js
@@ -1,6 +1,6 @@
 const Opening = () => {
   return (
-    <section className=" min-h-[30vh] flex justify-center py-20 lg:py-28  relative overflow-hidden">
+    <section className=" min-h-[30vh] flex justify-center py-20  lg:py-28 lg:pt-48 relative overflow-hidden">
       <div className="flex flex-col justify-center items-center relative z-10 px-10">
         <h1 className="text-bold text-violet-900 ">Leandro Soria</h1>
         <h2 className="text-violet-600 ">Front-End Developer</h2>
