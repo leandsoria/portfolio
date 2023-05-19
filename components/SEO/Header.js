1dc9136915da01584fa88953d9bb78e0842e595e
leandsoria <lean.dsoria@gmail.com>
2023-05-19 00:36:00 -0300

    MERGE :rocket:

--- components/SEO/Header.js
+++ components/SEO/Header.js
@@ -4,107 +4,138 @@ const Header = () => {
   return (
     <div>
       <Head>
-        <title>Leandro Soria | Front-End Developer</title>
-        <meta name="title" content="Leandro Soria | Front-End Developer" />
-        <meta name="description" content="Hi, welcome to my portfolio!" />
+        <title>
+          Leandro Soria - Front-End Developer | React - Next.js - Tailwind
+        </title>
+        <meta
+          name="title"
+          content="Leandro Soria - Front-End Developer | React - Next.js - Tailwind"
+        />
+        <meta
+          name="description"
+          content="Looking for a skilled Front-End Developer with expertise in React, Next.js, Tailwind, and CSS? I specialize in creating captivating user interfaces and immersive web experiences. With a strong command of React's component-based architecture, I build scalable and robust applications. Let's connect!"
+        />
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://leandsoria.vercel.app/" />
         <meta
           property="og:title"
-          content="Leandro Soria | Front-End Developer"
+          content="Leandro Soria - Front-End Developer | React - Next.js - Tailwind"
         />
         <meta
           property="og:description"
-          content="Hi, welcome to my portfolio!"
+          content="Looking for a skilled Front-End Developer with expertise in React, Next.js, Tailwind, and CSS? I specialize in creating captivating user interfaces and immersive web experiences. With a strong command of React's component-based architecture, I build scalable and robust applications. Let's connect!"
         />
         <meta
           property="og:image"
-          content="https://leandsoria.vercel.app/experiences/freelance_front-end.jpg"
+          content="https://leandsoria.vercel.app/meta/card.jpg"
         />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://leandsoria.vercel.app/" />
         <meta
           property="twitter:title"
-          content="Leandro Soria | Front-End Developer"
+          content="Leandro Soria - Front-End Developer | React - Next.js - Tailwind"
         />
         <meta
           property="twitter:description"
-          content="Hi, welcome to my portfolio!"
+          content="Looking for a skilled Front-End Developer with expertise in React, Next.js, Tailwind, and CSS? I specialize in creating captivating user interfaces and immersive web experiences. With a strong command of React's component-based architecture, I build scalable and robust applications. Let's connect!"
         />
         <meta
           property="twitter:image"
-          content="https://leandsoria.vercel.app/experiences/freelance_front-end.jpg"
+          content="https://leandsoria.vercel.app/experiences/meta/card.jpg"
         />
         <link
-          rel="icon"
-          sizes="192x192"
-          type="image/png"
-          href="/favicon/android-icon-192x192-seochecker-805.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-icon-180x180-seochecker-805.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/favicon/apple-icon-152x152-seochecker-805.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/favicon/apple-icon-144x144-seochecker-805.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/favicon/apple-icon-120x120-seochecker-805.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/favicon/apple-icon-114x114-seochecker-805.png"
-        />
-        <link
-          rel="icon"
-          sizes="96x96"
-          type="image/png"
-          href="/favicon/favicon-96x96-seochecker-805.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/favicon/apple-icon-76x76-seochecker-805.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/favicon/apple-icon-72x72-seochecker-805.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/favicon/apple-icon-60x60-seochecker-805.png"
-        />
-        <link
-          rel="apple-touch-icon"
+          rel="apple-touch-icon-precomposed"
           sizes="57x57"
-          href="/favicon/apple-icon-57x57-seochecker-805.png"
+          href="apple-touch-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="114x114"
+          href="/favicon/apple-touch-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="72x72"
+          href="/favicon/apple-touch-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="144x144"
+          href="/favicon/apple-touch-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="60x60"
+          href="/favicon/apple-touch-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="120x120"
+          href="/favicon/apple-touch-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="76x76"
+          href="/favicon/apple-touch-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="152x152"
+          href="/favicon/apple-touch-icon-152x152.png"
         />
         <link
           rel="icon"
+          type="image/png"
+          href="/favicon/favicon-196x196.png"
+          sizes="196x196"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-32x32.png"
           sizes="32x32"
-          type="image/png"
-          href="/favicon/favicon-32x32-seochecker-805.png"
         />
         <link
           rel="icon"
-          sizes="16x16"
           type="image/png"
-          href="/favicon/favicon-16x16-seochecker-805.png"
+          href="/favicon/favicon-16x16.png"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-128.png"
+          sizes="128x128"
+        />
+        <meta name="application-name" content="&nbsp;" />
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta
+          name="msapplication-TileImage"
+          content="/favicon/mstile-144x144.png"
+        />
+        <meta
+          name="msapplication-square70x70logo"
+          content="/favicon/mstile-70x70.png"
+        />
+        <meta
+          name="msapplication-square150x150logo"
+          content="/favicon/mstile-150x150.png"
+        />
+        <meta
+          name="msapplication-wide310x150logo"
+          content="/favicon/mstile-310x150.png"
+        />
+        <meta
+          name="msapplication-square310x310logo"
+          content="/favicon/mstile-310x310.png"
         />
       </Head>
     </div>
